e1ce37b9235be90879e6c11b399b0d1765186933
srikanthpl <sri1211p@gmail.com>
2023-10-10 23:54:52 +0530

    done

--- DJango/newpro/newpro/urls.py
+++ DJango/newpro/newpro/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Financy project.
+URL configuration for newpro project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from Loan import urls
+from django.urls import path
+from proj import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",include("Loan.urls"))
-    
+    path("index/",views.index)
 ]
